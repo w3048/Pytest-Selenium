9812389c73395c39165c53aaf1f0288707cb316d
Eugeniy A. Korge <29kea02@gmail.com>
2021-03-13 14:17:07 +0300

    Adding test for checking 'Is login link presented' on the page, and starting to use Page Object model in project

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,15 @@
+from pages.main_page import MainPage
+
+
+def test_guest_should_see_login_loink(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link) 
+    page.open()
+    page.should_be_login_link()
+    
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link) # inicialization Page Object
+    page.open() # method from base_page
+    page.go_to_login_page() # method from main_page.py
+    
