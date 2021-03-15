312f40bfc031b8b4e28cb5516e6f15e1c0a0fd5d
Eugeniy A. Korge <29kea02@gmail.com>
2021-03-15 14:42:59 +0300

    Add test that we can go to the login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,16 @@
-from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+from .base_page import BasePage
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        print('Founded login link...')
         login_link.click()
+        print('\nGo to login page...\n')
+                    
     def should_be_login_link(self):
-        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link isn't presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link isn't presented"
+        
--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,20 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
-def test_guest_should_see_login_loink(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link) 
     page.open()
     page.should_be_login_link()
     
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link) # inicialization Page Object
     page.open() # method from base_page
-    page.go_to_login_page() # method from main_page.py
+    print('Searching login link...\n')
+    page.go_to_login_page()
     
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+ 
